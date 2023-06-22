09769d9c08893aee53fded15752adec07831f23c
FontellaH <136127924+FontellaH@users.noreply.github.com>
2023-06-22 07:14:13 -0700

    updated script2.js code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var likes = document.querySelector("#liked-1");
-var topCount = 3 ;
+var topCount = 3;
 
 function liked() {
     topCount++;
     console.log(topCount);
-    likes.innerText = topCount + " like(s)";  
+    likes.innerText = topCount + " like(s)";
 }
